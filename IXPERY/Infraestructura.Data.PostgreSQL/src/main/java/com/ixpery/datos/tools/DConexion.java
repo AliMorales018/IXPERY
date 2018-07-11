040ac346af42977ee773e622859a07aa258bc8a1
XxZeroCoolxX <38397714+XxZeroCoolxX@users.noreply.github.com>
2018-07-11 11:48:35 -0700

    COTI-5

--- IXPERY/Infraestructura.Data.PostgreSQL/src/main/java/com/ixpery/datos/tools/DConexion.java
+++ IXPERY/Infraestructura.Data.PostgreSQL/src/main/java/com/ixpery/datos/tools/DConexion.java
@@ -7,6 +7,12 @@ public class DConexion {
     }
 
     public Datacnx ConectarBD() throws Exception{
-        return new Datacnx("192.168.0.20", "5432", "IXPERY2", "postgres", "admin");
+        return new Datacnx("192.168.0.18", "5432", "IXPERY2", "postgres", "admin");
     }
+
+
+
+
+
+
 }
